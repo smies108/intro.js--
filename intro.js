f54fd9b170eeb4812b3db69e0ba5840f56c9b344
rejoice <joicesmiles17@gmail.com>
2026-02-13 19:04:12 -0800

    Add intro.js asssignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+// Declare variables
+let name = "Edomwonyi Rejoice";
+let age = 18; // Change to your actual age
+let favoriteLanguage = "JavaScript";
+
+// Log the information to the console
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+console.log("My favorite Programming Language: " + favoriteLanguage);
